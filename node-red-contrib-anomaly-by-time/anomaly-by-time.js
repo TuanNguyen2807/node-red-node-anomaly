b30da3fef26aba07fa1765f6d997e1cbf79868df
Nguyễn Tuấn <79234967+TuanNguyen2807@users.noreply.github.com>
2022-03-28 16:21:24 +0700

    Add files via upload

---
+++ node-red-contrib-anomaly-by-time/anomaly-by-time.js
@@ -0,0 +1,246 @@
+module.exports = function(RED) {
+
+    var jsonata = require('jsonata');
+    
+    // function Compareeq(a, b) {
+    //     return (a == b) ? true : false;
+    // }
+    const getDateFromTimeString = str => new Date(null, null, null, ...str.split(':').map(t => parseInt(t)))
+    
+    function Comparegt(a, b) {
+        return (a > b) ? true : false;
+    }
+    // function Comparegte(a, b) {
+    //     return (a >= b) ? true : false;
+    // }
+    // function Comparelt(a, b) {
+    //     return (a < b) ? true : false;
+    // }
+    function Comparelte(a, b) {
+        return (a <= b) ? true : false;
+    }
+
+    // function CheckNode(config) {
+    //     RED.nodes.createNode(this,config);
+    //     this.start = config.start;
+    //     this.end = config.end;
+    //     this.volume = config.volume;
+    //     this.compare = config.compare;
+    //     this.errorMsg = msg => {
+    //         this.error(msg)
+    //         this.status({ fill: 'red', shape: 'dot', text: msg })
+    //         setTimeout(() => this.status({}), 10000)
+    //       }
+          
+    // this.validateDates = function (startDate, endDate) {
+    //     if (startDate >= endDate) {
+    //       this.errorMsg('start time >= end time')
+    //       return false
+    //     }
+    //     return true
+    //   }
+    //   if (!/^\d\d:\d\d:\d\d$/.test(this.start) || !/^\d\d:\d\d:\d\d$/.test(this.end)) {
+    //     this.errorMsg('Malformed time')
+    //     return false
+    //   }
+    //   this.startDate = getDateFromTimeString(this.start)
+    //   this.endDate = getDateFromTimeString(this.end)
+    //   this.validateDates(this.startDate, this.endDate)
+    //     var node = this;
+
+    //     // var timenow = new Date().getHours();
+        
+    //     node.on('input', function(msg) {
+    //         if (!node.validateDates(node.startDate, node.endDate)) return
+    //         const now = Date.now()
+    //         const start = (new Date(now)).setHours(node.startDate.getHours(), node.startDate.getMinutes(), node.startDate.getSeconds(), 0)
+    //         const end = (new Date(now)).setHours(node.endDate.getHours(), node.endDate.getMinutes(), node.endDate.getSeconds(), 0)
+    //         var strType = "NULL"
+    //         // if(typeof msg.payload !=="string"){
+    //         //     console.log("QUANG STRING")
+                
+    //         // } else {
+                
+    //         // }
+        
+    //         var inputJson = JSON.parse(msg.payload)
+    //             if(inputJson.length>0){
+    //                 strType = inputJson[0].bn;
+    //             }
+    //             var volume
+    //             if (typeof strType !== 'undefined' && strType!="NULL"){
+    //                 var typeLog = strType.split("_")[2] + strType.split("_")[3]
+    //                 if (typeLog==="ControlSetVolume"){
+    //                     volume = inputJson[0].v
+    //                     var mcuid = inputJson[1].vs
+    //                     if (now >= start && now < end) {
+    //                         switch (node.compare) {
+    //                             // case "eq" :
+    //                             //     if(Compareeq(vl, node.volume)) {
+    //                             //         msg.payload = "";
+    //                             //         node.send(msg);
+    //                             //     } else {
+    //                             //         node.warn("Not equal");
+    //                             //     }
+    //                             //     break;
+    //                             case "gt":
+    //                                 if(Comparegt(volume, node.volume)) {
+    //                                     msg.payload = "Phát hiện điều khiển âm thanh loa "+mcuid+" quá ngưỡng trong khoảng thời gian: " + this.start +" - " + this.end;
+    //                                     node.send(msg);
+    //                                 } 
+    //                                 else {
+    //                                     node.warn("Âm thanh điều khiển loa "+mcuid+" không vượt quá ngưỡng");
+    //                                 }
+    //                                 break;
+    //                             // case "gte":
+    //                             //     if(Comparegte(vl, node.volume)) {
+    //                             //         msg.payload = true;
+    //                             //         node.send(msg);
+    //                             //     } else {
+    //                             //         node.warn("Not greater than or equal");
+    //                             //     }
+    //                             //     break;
+    //                             // case "lt":
+    //                             //     if(Comparelt(vl, node.volume)) {
+    //                             //         msg.payload = true;
+    //                             //         node.send(msg);
+    //                             //     } else {
+    //                             //         node.warn("Not less than");
+    //                             //     }
+    //                             //     break;
+    //                             case "lte":
+    //                                 if(Comparelte(volume, node.volume)) {
+    //                                     msg.payload = "Phát hiện điều khiển âm thanh loa "+mcuid+" dưới ngưỡng trong khoảng thời gian: " + this.start +" - " + this.end;
+    //                                     node.send(msg);
+    //                                 }
+    //                                  else {
+    //                                     node.warn("Âm thanh điều khiển loa "+mcuid+" không dưới ngưỡng");
+    //                                 }
+    //                                 break;
+    //                         }
+    //                     } else {
+    //                         node.warn("Thời gian hiện tại không nằm trong khoảng "+ this.start +" - " + this.end);
+    //                     }
+    //                 }
+    //             }
+    //         // var vsString = msg.payload[0].vs;
+    //         // var vs,tmp
+
+        
+
+    //     });
+    // }
+
+    function CheckNodeAnomalyByTime(config) {
+        RED.nodes.createNode(this,config);
+        this.start = config.start;
+        this.end = config.end;
+        this.chosenID = config.chosenID;
+        this.chosenValue = config.chosenValue;
+        this.threshold = config.threshold;
+        this.compare = config.compare;
+        this.errorMsg = msg => {
+            this.error(msg)
+            this.status({ fill: 'red', shape: 'dot', text: msg })
+            setTimeout(() => this.status({}), 10000)
+          }
+          
+        this.validateDates = function (startDate, endDate) {
+            if (startDate >= endDate) {
+                this.errorMsg('start time >= end time')
+                return false
+            }
+                return true
+        }
+        if (!/^\d\d:\d\d:\d\d$/.test(this.start) || !/^\d\d:\d\d:\d\d$/.test(this.end)) {
+            this.errorMsg('Malformed time')
+            return false
+        }
+        this.startDate = getDateFromTimeString(this.start)
+        this.endDate = getDateFromTimeString(this.end)
+        this.validateDates(this.startDate, this.endDate)
+        var node = this;
+
+        // var timenow = new Date().getHours();
+        
+        node.on('input', async function(msg) {
+            try {
+                if (!node.validateDates(node.startDate, node.endDate)) return;
+                const now = Date.now()
+                const start = (new Date(now)).setHours(node.startDate.getHours(), node.startDate.getMinutes(), node.startDate.getSeconds(), 0)
+                const end = (new Date(now)).setHours(node.endDate.getHours(), node.endDate.getMinutes(), node.endDate.getSeconds(), 0)
+
+                let expResultID = await jsonata(node.chosenID).evaluate(msg.payload);
+                let expResultValue = await jsonata(node.chosenValue).evaluate(msg.payload);
+
+                if (typeof expResultID  === "object" || 
+                    typeof expResultValue === "object") {
+                    msg.payload = "ID or value must be a specific value.";
+                    node.send(msg);
+                    return;
+                } else if (typeof expResultID  === "undefined" || 
+                    typeof expResultValue === "undefined") {
+                    msg.payload = "ID or value is undefined.";
+                    node.send(msg);
+                    return;
+                } else if (isNaN(Number(expResultValue))) {
+                    msg.payload = "Value muse be a number.";
+                    node.send(msg);
+                    return;
+                } else {
+                    if (now >= start && now < end) {
+                        switch (node.compare) {
+                            // case "eq" :
+                            //     if(Compareeq(vl, node.volume)) {
+                            //         msg.payload = "";
+                            //         node.send(msg);
+                            //     } else {
+                            //         node.warn("Not equal");
+                            //     }
+                            //     break;
+                            case "gt":
+                                if(Comparegt(expResultValue, Number(node.threshold))) {
+                                    msg.payload = "Phát hiện thiết bị " + expResultID + " quá ngưỡng trong khoảng thời gian: " + this.start +" - " + this.end;
+                                    node.send(msg);
+                                } 
+                                else {
+                                    node.warn("Thiết bị " + expResultID + " không vượt quá ngưỡng");
+                                }
+                                break;
+                            // case "gte":
+                            //     if(Comparegte(vl, node.volume)) {
+                            //         msg.payload = true;
+                            //         node.send(msg);
+                            //     } else {
+                            //         node.warn("Not greater than or equal");
+                            //     }
+                            //     break;
+                            // case "lt":
+                            //     if(Comparelt(vl, node.volume)) {
+                            //         msg.payload = true;
+                            //         node.send(msg);
+                            //     } else {
+                            //         node.warn("Not less than");
+                            //     }
+                            //     break;
+                            case "lte":
+                                if(Comparelte(expResultValue, Number(node.threshold))) {
+                                    msg.payload = "Phát hiện thiết bị " + expResultID + " dưới ngưỡng trong khoảng thời gian: " + this.start +" - " + this.end;
+                                    node.send(msg);
+                                }
+                                    else {
+                                    node.warn("Thiết bị " + expResultID + " không dưới ngưỡng");
+                                }
+                                break;
+                        }
+                    } else {
+                        node.warn("Thời gian hiện tại không nằm trong khoảng "+ this.start +" - " + this.end);
+                    }
+                }
+            } catch (error) {
+                console.log("Error: ", error);
+            }
+        });
+    }
+    RED.nodes.registerType("anomaly by time", CheckNodeAnomalyByTime);
+}
---
+++ node-red-contrib-anomaly-by-value/anomaly-by-value.js
@@ -0,0 +1,176 @@
+module.exports = function(RED) {
+
+    var jsonata = require('jsonata');
+
+    // function Compareeq(a, b) {
+    //     return (a == b) ? true : false;
+    // }
+    function Comparegt(a, b) {
+        return (a > b) ? true : false;
+    }
+    // function Comparegte(a, b) {
+    //     return (a >= b) ? true : false;
+    // }
+    // function Comparelt(a, b) {
+    //     return (a < b) ? true : false;
+    // }
+    function Comparelte(a, b) {
+        return (a <= b) ? true : false;
+    }
+
+    // function CheckNode(config) {
+    //     RED.nodes.createNode(this,config);
+    //     this.temp = config.temp;
+    //     this.compare = config.compare;
+
+    //     var node = this;
+
+    //     node.on('input', function(msg) {
+    //         var inputJson = JSON.parse(msg.payload)
+    //         var strType = "NULL"
+    //         if(inputJson.length>0){
+    //             strType = inputJson[0].bn
+    //         }
+    //         if (typeof strType !== 'undefined' && strType!="NULL"){
+    //             var typeLog = strType.split("_")[2] + strType.split("_")[3]
+    //             if (typeLog==="OPUSTATUS"){
+    //                 var tmp =  inputJson[1].v
+    //                 var mcu_id = inputJson[0].vs
+    //                 switch (node.compare) {
+    //                     // case "eq" :
+    //                     //     if(Compareeq(vl, node.volume)) {
+    //                     //         msg.payload = "";
+    //                     //         node.send(msg);
+    //                     //     } else {
+    //                     //         node.warn("Not equal");
+    //                     //     }
+    //                     //     break;
+    //                     case "gt":
+    //                         if(Comparegt(tmp, node.temp)) {
+    //                             msg.payload = "Loa "+mcu_id+" có nhiệt độ vượt ngưỡng cho phép!";
+    //                             node.send(msg);
+    //                         } 
+    //                         // else {
+    //                         //     node.warn("");
+    //                         // }
+    //                         break;
+    //                     // case   "gte":
+    //                     //     if(Comparegte(vl, node.volume)) {
+    //                     //         msg.payload = true;
+    //                     //         node.send(msg);
+    //                     //     } else {
+    //                     //         node.warn("Not greater than or equal");
+    //                     //     }
+    //                     //     break;
+    //                     // case "lt":
+    //                     //     if(Comparelt(vl, node.volume)) {
+    //                     //         msg.payload = true;
+    //                     //         node.send(msg);
+    //                     //     } else {
+    //                     //         node.warn("Not less than");
+    //                     //     }
+    //                     //     break;
+    //                     case "lte":
+    //                         if(Comparelte(tmp, node.temp)) {
+    //                             msg.payload = "Loa "+mcu_id+" có nhiệt độ nhỏ hơn ngưỡng cho phép!";
+    //                             node.send(msg);
+    //                         } 
+    //                         // else {
+    //                         //     node.warn("Not less than or equal");
+    //                         // }
+    //                         break;
+    //                 }
+    //             }
+    //         }
+    //     });
+    // }
+
+    function CheckNodeAnomalyByValue(config) {
+        RED.nodes.createNode(this,config);
+        this.name = config.name;
+        this.threshold = config.threshold;
+        this.compare = config.compare;
+        this.chosenID = config.chosenID;
+        this.chosenValue = config.chosenValue;
+
+        var node = this;
+
+        node.on('input', async function(msg) {
+            try {
+                if (node.chosenID === "" || 
+                node.chosenValue === "") {
+                    msg.payload = "ID and value must not blank.";
+                    node.send(msg);
+                    return;
+                }
+
+                let expResultID = await jsonata(node.chosenID).evaluate(msg.payload);
+                let expResultValue = await jsonata(node.chosenValue).evaluate(msg.payload);
+
+                if (typeof expResultID  === "object" || 
+                    typeof expResultValue === "object") {
+                    msg.payload = "ID or value must be a specific value.";
+                    node.send(msg);
+                    return;
+                } else if (typeof expResultID  === "undefined" || 
+                    typeof expResultValue === "undefined") {
+                    msg.payload = "ID or value is undefined.";
+                    node.send(msg);
+                    return;
+                } else if (isNaN(Number(expResultValue))) {
+                    msg.payload = "Value muse be a number.";
+                    node.send(msg);
+                    return;
+                } else {
+                    switch (node.compare) {
+                        // case "eq" :
+                        //     if(Compareeq(vl, node.volume)) {
+                        //         msg.payload = "";
+                        //         node.send(msg);
+                        //     } else {
+                        //         node.warn("Not equal");
+                        //     }
+                        //     break;
+                        case "gt":
+                            if(Comparegt(Number(expResultValue), Number(node.threshold))) {
+                                msg.payload = "Loa " + expResultID  + " có nhiệt độ vượt ngưỡng cho phép!";
+                                node.send(msg);
+                            } 
+                            // else {
+                            //     node.warn("");
+                            // }
+                            break;
+                        // case "gte":
+                        //     if(Comparegte(vl, node.volume)) {
+                        //         msg.payload = true;
+                        //         node.send(msg);
+                        //     } else {
+                        //         node.warn("Not greater than or equal");
+                        //     }
+                        //     break;
+                        // case "lt":
+                        //     if(Comparelt(vl, node.volume)) {
+                        //         msg.payload = true;
+                        //         node.send(msg);
+                        //     } else {
+                        //         node.warn("Not less than");
+                        //     }
+                        //     break;
+                        case "lte":
+                            if(Comparelte(Number(expResultValue), Number(node.threshold))) {
+                                msg.payload = "Loa " + expResultID + " có nhiệt độ nhỏ hơn ngưỡng cho phép!";
+                                node.send(msg);
+                            } 
+                            // else {
+                            //     node.warn("Not less than or equal");
+                            // }
+                            break;
+                    }
+                }
+            } catch (error) {
+                console.log("ERRORRRRRRRRR", error);
+            }
+        });
+    }
+    RED.nodes.registerType("anomaly by value", CheckNodeAnomalyByValue);
+}
